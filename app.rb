fbbe63a180034ac8f1fb3319cd07bb64dc12acf2
klocode <kendrick.t.lo@gmail.com>
2017-02-24 15:58:19 -0500

    working on all of the explorer tests

--- app.rb
+++ app.rb
@@ -1,10 +1,42 @@
 require 'sinatra'
 require 'active_record'
 require 'sqlite3'
+require 'pry'
 
 ActiveRecord::Base.establish_connection(
   adapter:  'sqlite3',
   database: 'db/development.sqlite3'
 )
 
+get "/api/notes.json" do
+  notes = Note.all
+  notes.to_json
+end
 
+get "/api/notes/tag/:tags" do
+  # pulls every note that includes the [:tag]
+  tagg = Tag.find_by(name: params[:tags])
+  if tagg
+    tagg.to_json
+  end
+end
+
+
+post "/api/notes" do
+  @note = Note.new(
+  title: params[:title],
+  body: params[:body]
+  )
+  if @note.save
+    #put note
+    #split on the comma, strip spaces out &, loop over it and pump it into notes[tag] column
+    #find and create a tag for that
+    notes.tag << Tag.find_or_create_by(name: params[:name])
+    #notes.tag << tag object
+  else
+    status 400
+    # render hash to json
+    errors = {error: "Title can't be blank"}
+    errors
+  end
+end
---
+++ models/note.rb
@@ -0,0 +1,18 @@
+class Note < ActiveRecord::Base
+
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+
+  validates :title, presence: true
+  validates :body, presence: true
+
+  def as_json(arg = nil)
+    {
+      title: title,
+      body: body,
+      tags: tags.map{|tag| {name: tag.name}}
+    }
+  end
+
+end
